d90302a3acab227b387b201d490d1a2cb9716dc6
Saurabh-nimblework <saurabh.nandwal@nimblework.com>
2023-08-30 15:39:50 +0530

    sideNav

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -1,9 +1,50 @@
+import { Drawer, Box, Toolbar, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import HelpIcon from '@mui/icons-material/Help';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+
+const drawerWidth = 280;
+
+const listItem = [
+    {
+        text: 'Project',
+        icon : <AssignmentOutlinedIcon/>
+    },
+    {
+        text: 'Help',
+        icon : <HelpIcon/>
+    },
+];
 
 export const SideNav = () => {
+
     return (
         <>
-        
-        <p>In the Side Nav</p>
+          <Drawer
+            sx={{
+                '& .MuiDrawer-paper': {
+                  width: drawerWidth,
+                  boxSizing: 'border-box',
+                  backgroundColor: '#145496'
+                },
+            }}
+            anchor="left"
+            variant="permanent" 
+          >
+            <Toolbar></Toolbar>
+            <Divider/>  
+            <List>
+            {listItem.map((item) => (
+              <ListItem key={item}>
+              <ListItemButton href="#">
+                  <ListItemIcon sx={{color: '#fff'}}>
+                    {item.icon}
+                  </ListItemIcon>
+                  <ListItemText sx={{color:'white', fontWeight: 'medium' }} primary={item.text} />
+              </ListItemButton>
+              </ListItem>
+            ))}
+        </List>
+          </Drawer>
         </>
     );
 }
